7c8d182f8dc1d282cc6fbb4fc870f908f54b4cf9
bpbp <Oosik@users.noreply.github.com>
2020-11-03 10:43:05 -0600

    Fix path errors
    Switch to absolute paths. Also add a check to make sure the user is 
    calling the script from the root WP directory. Did some clean up and 
    documentation as well.

--- wp-acf-install.sh
+++ wp-acf-install.sh
@@ -4,8 +4,38 @@
 ## add your pro license key to the .env file
 ## then setup an alias and point to this file.
 ## !! wpcli is required to run this script !!
-source '.env'
-wp option add acf_pro_license ${ACF_KEY} --autoload=yes
-wget -O wp-content/plugins/acf.zip "http://connect.advancedcustomfields.com/index.php?p=pro&a=download&k=${ACF_KEY}"
-wp plugin install wp-content/plugins/acf.zip --activate
-rm wp-content/plugins/acf.zip
+
+##
+## Set the paths
+DIR_PATH=$(dirname $0)
+CURRENT_PATH=$(pwd)
+
+##
+## make sure we are in the wp root dir
+if [ ! -f "${CURRENT_PATH}/wp-config.php" ]
+then
+	echo ""
+	echo -e "\e[31mCan't find wp-config, you need to run this command from root WordPress directory\e[0m"
+	echo ""
+	exit
+fi
+
+##
+## add in env vars
+source "${DIR_PATH}/.env"
+
+##
+## add key to DB
+wp option add acf_pro_license "${ACF_KEY}" --autoload=yes
+
+##
+## dowload the plugin
+wget -O "${CURRENT_PATH}/wp-content/plugins/acf.zip" "http://connect.advancedcustomfields.com/index.php?p=pro&a=download&k=${ACF_KEY}"
+
+##
+## install and activate.
+wp plugin install "${CURRENT_PATH}/wp-content/plugins/acf.zip" --activate
+
+##
+## clean up
+rm "${CURRENT_PATH}/wp-content/plugins/acf.zip"
